eb9aed50fc6d3e9c90a84a644c4441a8a272344b
Matthew Christensen <mchristensen@moosepod.com>
2017-01-05 19:11:43 -0500

    Fixed map generator. Made better map.

--- jan_2016/matt/mapmaker.py
+++ jan_2016/matt/mapmaker.py
@@ -2,15 +2,41 @@
     Needs to always output 192 lines with some buffer on top/bottom. Maps have a width of 20 and are reflected """
 
 TOTAL_HEIGHT=192
-SCOREBOARD_HEIGHT=11
+SCOREBOARD_HEIGHT=12
 BORDER_WIDTH=10
 
+MAP_ROW_SIZE=20
+
+MAP_DATA=['x......x..',
+		  'x..x.x...x',
+		  'x..xxxxxxx',
+		  'x.........',
+		  'xxxxxxxx.x',
+		  'x......x.x',
+		  'x.xxxxxx.x',
+		  'x.........']
+
+def parse_map_data(data):
+	rows = []
+	for line in data:
+		row = []
+		rows.append(row)
+		cells = line.replace('.','0').replace('x','1')
+		row.extend((cells[1],cells[1],cells[0],cells[0]))
+		row.extend((cells[2],cells[2],cells[3],cells[3],cells[4],cells[4],cells[5],cells[5]))
+		row.extend((cells[9],cells[9],cells[8],cells[8],cells[7],cells[7],cells[6],cells[6]))
+	return rows
+
 def main():
+	map_data = parse_map_data(MAP_DATA)
+	map_data.reverse() # byte order should be opposite of visual ordering 
+	print 
 	print "PFData0"
 	for i in range(0,BORDER_WIDTH):
 		print ' .byte #%11110000'
-	for i in range(0,TOTAL_HEIGHT-SCOREBOARD_HEIGHT-BORDER_WIDTH-BORDER_WIDTH):
-		print ' .byte #%00010000'
+	for row in map_data:
+		for i in range(0,MAP_ROW_SIZE):
+			print ' .byte #%' + ''.join(row[0:4]) + '0000'
 	for i in range(0,BORDER_WIDTH):
 		print ' .byte #%11110000'
 	for i in range(0,SCOREBOARD_HEIGHT):
@@ -20,8 +46,9 @@ def main():
 	print "PFData1"
 	for i in range(0,BORDER_WIDTH):
 		print ' .byte #%11111111'
-	for i in range(0,TOTAL_HEIGHT-SCOREBOARD_HEIGHT-BORDER_WIDTH-BORDER_WIDTH):
-		print ' .byte #%00000000'
+	for row in map_data:
+		for i in range(0,MAP_ROW_SIZE):
+			print ' .byte #%' + ''.join(row[4:12])
 	for i in range(0,BORDER_WIDTH):
 		print ' .byte #%11111111'
 	for i in range(0,SCOREBOARD_HEIGHT):
@@ -31,8 +58,9 @@ def main():
 	print "PFData2"
 	for i in range(0,BORDER_WIDTH):
 		print ' .byte #%11111111'
-	for i in range(0,TOTAL_HEIGHT-SCOREBOARD_HEIGHT-BORDER_WIDTH-BORDER_WIDTH):
-		print ' .byte #%00000000'
+	for row in map_data:
+		for i in range(0,MAP_ROW_SIZE):
+			print ' .byte #%' + ''.join(row[12:20]	)
 	for i in range(0,BORDER_WIDTH):
 		print ' .byte #%11111111'
 	for i in range(0,SCOREBOARD_HEIGHT):
