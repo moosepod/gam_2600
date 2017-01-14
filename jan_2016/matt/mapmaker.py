7a92bac933562dacf6e92440d2b1006129e0fbd3
Matthew Christensen <mchristensen@moosepod.com>
2017-01-14 15:11:48 -0500

    Fixed offsets with additional map data

--- jan_2016/matt/mapmaker.py
+++ jan_2016/matt/mapmaker.py
@@ -7,7 +7,9 @@ BORDER_WIDTH=10
 
 MAP_ROW_SIZE=20
 
-MAP_DATA=['x......x..',
+# x is a base wall
+# o is a wall only on the reflected side. can only be used in columns 2-4 (0-indexed)
+MAP_DATA=['x..o...x..',
 		  'x..x.x...x',
 		  'x..xxxxxxx',
 		  'x.........',
@@ -21,10 +23,12 @@ def parse_map_data(data):
 	for line in data:
 		row = []
 		rows.append(row)
-		cells = line.replace('.','0').replace('x','1')
+		cells = line.replace('.','0').replace('x','1').replace('o','0')
 		row.extend((cells[1],cells[1],cells[0],cells[0]))
 		row.extend((cells[2],cells[2],cells[3],cells[3],cells[4],cells[4],cells[5],cells[5]))
 		row.extend((cells[9],cells[9],cells[8],cells[8],cells[7],cells[7],cells[6],cells[6]))
+		cells = line.replace('.','0').replace('x','1').replace('o','1')
+		row.extend((cells[2],cells[2],cells[3],cells[3],cells[4],cells[4],cells[5],cells[5]))
 	return rows
 
 def main():
@@ -66,6 +70,17 @@ def main():
 	for i in range(0,SCOREBOARD_HEIGHT):
 		print ' .byte #%00000000'
 
+	print ''
+	print "PFData3"
+	for i in range(0,BORDER_WIDTH):
+		print ' .byte #%11111111'
+	for row in map_data:
+		for i in range(0,MAP_ROW_SIZE):
+			print ' .byte #%' + ''.join(row[20:28])
+	for i in range(0,BORDER_WIDTH):
+		print ' .byte #%11111111'
+	for i in range(0,SCOREBOARD_HEIGHT):
+		print ' .byte #%00000000'
 
 
 if __name__ == "__main__":
