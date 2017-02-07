9aebb6fe18e802983eb4de7a160fe9d76f82e355
Matthew Christensen <mchristensen@moosepod.com>
2017-02-07 18:29:35 -0500

    Spiral maze

--- jan_2016/matt/mapmaker.py
+++ jan_2016/matt/mapmaker.py
@@ -5,31 +5,84 @@ MAP_ROW_SIZE=32
 
 # x is a base wall
 # o is a wall only on the reflected side. can only be used in columns 2-4 (0-indexed)
-#            cccc         cccxx              
-MAP_DATA=['x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
-		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
-		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
-		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
-		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
-		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
-		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
-		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
-		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
-		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
-		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
-		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
-		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
-		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
-		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
-		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
-		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
-		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
-		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
-		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
-		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
-		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
-		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
-		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x']
+#            cccc         cccxx      
+
+# Test pattern -- will form a checkerboard       
+# MAP_DATA=['x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
+# 		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
+# 		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
+# 		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
+# 		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
+# 		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
+# 		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
+# 		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
+# 		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
+# 		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
+# 		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
+# 		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
+# 		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
+# 		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
+# 		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
+# 		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
+# 		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
+# 		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
+# 		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
+# 		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
+# 		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
+# 		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x',
+# 		  'x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.',
+# 		  '.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x.x']
+
+# Template for making our own mazes
+# MAP_DATA=['xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+# 		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx']	
+
+MAP_DATA=['x.xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+		  'x.x............................x',
+		  'x.x.xxxxxxxxxxxxxxxxxxxxxxxxxx.x',
+		  'x.x.x.......................xx.x',
+		  'x.x.x.xxxxxxxxxxxxxxxxxxxxx.xx.x',
+		  'x.x.x.xx.................xx.xx.x',
+		  'x.x.x.xx.xxxxxxxxxxxxxxx.xx.xx.x',
+		  'x.x.x.xx.x............xx.xx.xx.x',
+		  'x.x.x.xx.x.xxxxxxxxxx.xx.xx.xx.x',
+		  'x.x.x.xx.x.x........x.xx.xx.xx.x',
+		  'x.x.x.xx.x.x.xxxxxx.x.xx.xx.xx.x',
+		  'x.x.x.xx.x.x.xxxx.x.x.xx.xx.xx.x',
+		  'x.x.x.xx.x.x......x.x.xx.xx.xx.x',
+		  'x.x.x.xx.x.xxxxxxxx.x.xx.xx.xx.x',
+		  'x.x.x.xx.x..........x.xx.xx.xx.x',
+		  'x.x.x.xx.xxxxxxxxxxxx.xx.xx.xx.x',
+		  'x.x.x.xx..............xx.xx.xx.x',
+		  'x.x.x.xxxxxxxxxxxxxxxxxx.xx.xx.x',
+		  'x.x.x....................xx.xx.x',
+		  'x.x.xxxxxxxxxxxxxxxxxxxxxxx.xx.x',
+		  'x.x.........................xx.x',
+		  'x.xxxxxxxxxxxxxxxxxxxxxxxxxxxx.x',
+		  'x..............................x',
+		  'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx']	
 
 def parse_map_data(data):
 	rows = []
@@ -57,12 +110,12 @@ def main():
 		print ' .byte #%' + ''.join(row[8:16]) 
 
 	print ''
-	print 'PFData4'
+	print 'PFData5'
 	for row in map_data:
 		print ' .byte #%' + ''.join(row[16:24]) 
 
 	print ''
-	print 'PFData5'
+	print 'PFData4'
 	for row in map_data:
 		print ' .byte #%' + ''.join(row[24:32]) 
 
