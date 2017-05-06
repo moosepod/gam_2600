514a0a2c95622be87c064b9f94c81e8c69c8302e
Matthew Christensen <mchristensen@moosepod.com>
2017-05-06 11:55:54 -0400

    Added states

--- may_2017/moosewar.asm
+++ may_2017/moosewar.asm
@@ -20,6 +20,12 @@
         seg.u Variables
         org $80
 
+GameState	byte ; Contains the main state of the game
+STATE_START   equ 0
+STATE_PLAYING equ 1
+STATE_P1_WIN  equ 2
+STATE_P2_WIN  equ 3
+
 Score0	byte	; BCD score of player 0
 Score1	byte	; BCD score of player 1
 
@@ -47,22 +53,27 @@ SuitSpriteH equ 8 ; horizontal position of suit sprite
 
 Start
     CLEAN_START
+
+	lda #STATE_START
+	sta GameState
+
 	lda #$00
 	sta Score0
 	lda #$00
 	sta Score1
 
-	lda #$09
-	sta Card0
-	lda #$01
-	sta Card1
-	
 NextFrame
 	VERTICAL_SYNC
 
 ; 37 lines BLANK
 	TIMER_SETUP 37
-	
+
+	; Check reset switch. Start game if triggered
+	lsr SWCHB ; This will shift bit 0 into Carry, which we can then test
+	bcs NoReset
+	jsr StartGame
+
+NoReset	
 	lda Score0
     ldx #0
 	jsr GetBCDBitmap
@@ -90,41 +101,92 @@ Sprite2Loop
     TIMER_WAIT	
 	TIMER_SETUP 50
 
-;;; SCOREBOARD
-; Put the playfield into score mode (bit 2) which gives
-; two different colors for the left/right side of
-; the playfield (given by COLUP0 and COLUP1).
-	lda #%00010010	; score mode + 2 pixel ball
-	sta CTRLPF
-	lda #$48
-	sta COLUP0	; set color for left
-	lda #$a8
-	sta COLUP1	; set color for right
-
-; Now we draw all four digits.
-	ldy #0		; Y will contain the frame Y coordinate
-ScanLoop1a
-	sta WSYNC
-	tya
-	lsr		; divide Y by two for double-height lines
-	tax		; -> X
-	lda FontBuf+0,x
-	sta PF1		; set left score bitmap
-	SLEEP 28
-	lda FontBuf+5,x
-	sta PF1		; set right score bitmap
-	iny
-	cpy #10
-	bcc ScanLoop1a
-
-;;;; Clear the playfield
-	lda #0
-	sta WSYNC
-	sta PF1
+	; Draw the score no matter what the state
+	jsr DrawScoreboard
 
     TIMER_WAIT	
 
-;;;;; Draw the card decks. 
+	; Jump to logic for each game state. We have 122 lines left at this point. Each
+	; subroutine must handle its own lines. Logic is to keep subtracting, and when x goes 
+	; below zero, jump to the appropriate subroutine
+	ldx GameState
+	dex 
+	bpl GameStateNext1
+	jmp StartStateKernel
+GameStateNext1 
+	dex
+	bpl GameStateLoopReturn
+	jmp PlayingStateKernel
+GameStateLoopReturn
+
+; 30 lines overscan
+	TIMER_SETUP 30
+    TIMER_WAIT	
+
+    jmp NextFrame
+
+;;;
+;;; Change the game state to started after initializing variables
+;;;
+StartGame subroutine
+	lda #STATE_PLAYING
+	sta GameState
+
+	; Initialize scores
+	lda #$00
+	sta Score0
+	lda #$00
+	sta Score1
+
+	; Initialize cards
+	lda #$09
+	sta Card0
+	lda #$01
+	sta Card1
+	
+	rts
+
+;;;
+;;; Kernel for game in initial starting state (no game started)
+;;;
+
+StartStateKernel
+	;;;;; Draw the card decks. 
+
+	lda #%00000011	; score mode + reflect playfield
+	sta CTRLPF
+
+	; top of cards
+	TIMER_SETUP 100
+	lda #$41
+	sta COLUPF
+
+	lda PFData0
+	sta PF0
+
+	lda PFData1
+	sta PF1
+
+	lda PFData2
+	sta PF2
+
+    TIMER_WAIT
+
+; Remaining lines -- 192-50-100. Draw no playfield
+	TIMER_SETUP 32
+	lda #%00
+	sta PF0
+	sta PF1
+	sta PF2
+	TIMER_WAIT
+	
+	jmp GameStateLoopReturn
+
+;;;
+;;; Kernel for the game in playing state
+;;;
+PlayingStateKernel
+	;;;;; Draw the card decks. 
 
 	lda #%00000011	; score mode + reflect playfield
 	sta CTRLPF
@@ -183,12 +245,44 @@ SpriteLoop
 	sta PF1
 	sta PF2
 	TIMER_WAIT
+	
+	jmp GameStateLoopReturn
 
-; 30 lines overscan
-	TIMER_SETUP 30
-    TIMER_WAIT	
+;;;
+;;; Draw the scoreboard
+;;;
+DrawScoreboard subroutine
+	; Put the playfield into score mode (bit 2) which gives
+	; two different colors for the left/right side of
+	; the playfield (given by COLUP0 and COLUP1).
+	lda #%00010010	; score mode + 2 pixel ball
+	sta CTRLPF
+	lda #$48
+	sta COLUP0	; set color for left
+	lda #$a8
+	sta COLUP1	; set color for right
 
-    jmp NextFrame
+	; Now we draw all four digits.
+	ldy #0		; Y will contain the frame Y coordinate
+ScanLoop1a
+	sta WSYNC
+	tya
+	lsr		; divide Y by two for double-height lines
+	tax		; -> X
+	lda FontBuf+0,x
+	sta PF1		; set left score bitmap
+	SLEEP 28
+	lda FontBuf+5,x
+	sta PF1		; set right score bitmap
+	iny
+	cpy #10
+	bcc ScanLoop1a
+
+	; Clear the playfield
+	lda #0
+	sta WSYNC
+	sta PF1
+	rts
 
 ; Fetches bitmap data for two digits of a
 ; BCD-encoded number, storing it in addresses
